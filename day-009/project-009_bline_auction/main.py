d9d6b680762ddb96aedc198dffbc0194d6d407ae
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-12-13 15:35:27 +0000

    feat(009): add start of main()

--- day-009/project-009_bline_auction/main.py
+++ day-009/project-009_bline_auction/main.py
@@ -3,3 +3,12 @@ import art
 # Welcome section
 print(art.logo)
 print("Welcome to the secret auction program.\n")
+
+
+# Main code
+def main():
+    bids = {}
+
+
+if __name__ == '__main__':
+    main()
