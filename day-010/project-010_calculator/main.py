2f7b6fb17b1001440a890ec9c591a7f8a589f990
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-12-16 14:20:38 +0000

    feat(010): allow calculations to carry over

--- day-010/project-010_calculator/main.py
+++ day-010/project-010_calculator/main.py
@@ -23,14 +23,23 @@ operations = {
 
 
 def main():
+    calculating = True
     num1 = int(input("What's the first number?: "))
-    num2 = int(input("What's the second number?: "))
-    for symbol in operations:
-        print(symbol)
-    operation_symbol = input("Pick an operation from the options above: ")
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num2)
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    while calculating:
+        num2 = int(input("What's the second number?: "))
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation from the options above: ")
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or "
+                       f"type 'n' to start a new calculation: ")
+        if choice == "y":
+            num1 = answer
+        else:
+            calculating = False
 
 
 if __name__ == "__main__":
