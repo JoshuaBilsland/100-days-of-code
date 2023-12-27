6a93a874dba175b9466ffeb98aac1ba8c11365fc
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-12-27 10:26:23 +0000

    feat(012): add getting random int

--- day-012/project-012_number_guessing_game/main.py
+++ day-012/project-012_number_guessing_game/main.py
@@ -8,13 +8,18 @@
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 import art
+import random
 
 EASY_GUESSES_REMAINING = 5
 HARD_GUESSES_REMAINING = 10
 
 
 def main():
-    print()
+    print(get_random_number())
+
+
+def get_random_number():
+    return random.randint(1, 100)
 
 
 if __name__ == "__main__":
