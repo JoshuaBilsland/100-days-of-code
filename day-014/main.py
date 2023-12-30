192b6767073f872a23cb01883aca8c224c636319
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-12-30 12:06:44 +0000

    feat(014): add choosing account

--- day-014/main.py
+++ day-014/main.py
@@ -1,10 +1,19 @@
 import art
+import random
+import game_data
 
 
 def main():
     current_score = 0
+    previous_account = get_account()
     winning = True
     while winning:
+        # Pick a new account to compare against
+        next_account_to_compare = get_account()
+        while next_account_to_compare == previous_account:
+            next_account_to_compare = get_account()
+        print(previous_account)
+        print(next_account_to_compare)
         choice = get_choice()
 
 
@@ -17,6 +26,10 @@ def get_choice():
         else:
             print("ERROR: Only enter 'A' or 'B'")
     return user_input
+
+
+def get_account():
+    return random.choice(game_data.data)
 
 
 if __name__ == "__main__":
