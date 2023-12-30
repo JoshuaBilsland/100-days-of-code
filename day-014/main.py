06bdfc2d415bb6d48374accef7fddf18e0124412
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-12-30 11:48:50 +0000

    feat(014): add getting user choice

--- day-014/main.py
+++ day-014/main.py
@@ -5,7 +5,18 @@ def main():
     current_score = 0
     winning = True
     while winning:
-        print()
+        choice = get_choice()
+
+
+def get_choice():
+    valid_input = False
+    while not valid_input:
+        user_input = input("Who has more followers? Type 'A' or 'B': ")
+        if user_input.upper() in ["A", "B"]:
+            valid_input = True
+        else:
+            print("ERROR: Only enter 'A' or 'B'")
+    return user_input
 
 
 if __name__ == "__main__":
