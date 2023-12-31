8d62abeb2c293b7d1c6a414be38258861f7b8009
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-12-31 10:07:15 +0000

    feat(015): add report

--- day-015/project-015_coffee_machine/main.py
+++ day-015/project-015_coffee_machine/main.py
@@ -2,6 +2,14 @@ import menu
 
 
 def main():
+    # 'a' stands for append the symbol, 'p' stands for prepend the symbol
+    resources = {
+        "Water": [300, "ml", "a"],
+        "Milk": [200, "ml", "a"],
+        "Coffee": [100, "g", "a"],
+        "Money": [0, "$", "p"]
+    }
+
     on = True
     while on:
         user_choice = input(
@@ -13,11 +21,19 @@ def main():
         elif user_choice == "cappuccino":
             print()
         elif user_choice == "report":
-            print()
+            report(resources)
         elif user_choice == "off":
             on = False
         else:
             print("ERROR: Invalid choice")
+
+
+def report(resources_dict):
+    for key in resources_dict:
+        if resources_dict[key][2] == "a":
+            print(f"{key}: {resources_dict[key][0]}{resources_dict[key][1]}")
+        elif resources_dict[key][2] == "p":
+            print(f"{key}: {resources_dict[key][1]}{resources_dict[key][0]}")
 
 
 if __name__ == "__main__":
