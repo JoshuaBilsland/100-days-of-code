cda02b8bf7f05578cfeef111f3331ed0caf8e27e
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-03 09:03:07 +0000

    feat(016): complete main loop

--- day-016/project-016_oop_coffee_machine/main.py
+++ day-016/project-016_oop_coffee_machine/main.py
@@ -5,11 +5,23 @@ from money_machine import MoneyMachine
 
 def main():
     coffee_maker = CoffeeMaker()
-    money_maker = MoneyMachine()
+    money_machine = MoneyMachine()
     menu = Menu()
     on = True
-    
+
     while on:
+        user_choice = input(f"What drink would you like? ({menu.get_items()}): ")
+        if user_choice == "report":
+            coffee_maker.report()
+            money_machine.report()
+        elif user_choice == "off":
+            on = False
+        else:
+            drink = menu.find_drink(user_choice)
+
+            if (coffee_maker.is_resource_sufficient(drink)
+                    and money_machine.make_payment(drink.cost)):
+                coffee_maker.make_coffee(drink)
 
 
 if __name__ == "__main__":
