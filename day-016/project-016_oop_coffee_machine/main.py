30522e5b50c2ab34de4bc426bd0eb56a4f814325
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-03 08:55:29 +0000

    feat(016): add main loop start

--- day-016/project-016_oop_coffee_machine/main.py
+++ day-016/project-016_oop_coffee_machine/main.py
@@ -1,3 +1,16 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+def main():
+    coffee_maker = CoffeeMaker()
+    money_maker = MoneyMachine()
+    menu = Menu()
+    on = True
+    
+    while on:
+
+
+if __name__ == "__main__":
+    main()
