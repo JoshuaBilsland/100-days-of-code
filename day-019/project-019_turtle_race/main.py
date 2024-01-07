dba2e4f49a284635df5b3894b128131b182a1eb6
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-07 08:49:23 +0000

    feat(019): add input prompt

--- day-019/project-019_turtle_race/main.py
+++ day-019/project-019_turtle_race/main.py
@@ -1,7 +1,13 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
-screen = Screen()
+
+def main():
+    screen.exitonclick()
 
 
-screen.exitonclick()
+if __name__ == "__main__":
+    tim = Turtle()
+    screen = Screen()
+    screen.setup(width=500, height=400)
+    screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a colour: ")
+    main()
