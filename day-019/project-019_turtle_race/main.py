3d580bef8068bff75c77ba27a7d41a3d5952887d
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-07 09:07:08 +0000

    feat(019): create the turtles

--- day-019/project-019_turtle_race/main.py
+++ day-019/project-019_turtle_race/main.py
@@ -1,13 +1,30 @@
 from turtle import Turtle, Screen
 
 
-def main():
+def main(colours):
+    turtles = []
+    y_positions = [-70, -40, -10, 20, 50, 80]
+    pos_index = 0
+    for colour in colours:
+        turtle = Turtle(shape="turtle")
+        turtle.color(colour)
+        turtles.append(turtle)
+        turtle.penup()
+        turtle.goto(x=-230, y=y_positions[pos_index])
+        pos_index += 1
+
     screen.exitonclick()
 
-
 if __name__ == "__main__":
-    tim = Turtle()
+    colours = [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "purple"
+    ]
     screen = Screen()
     screen.setup(width=500, height=400)
-    screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a colour: ")
-    main()
+    screen.textinput(title="Make your bet", prompt=f"Which turtle will win the race? {colours} Enter a colour: ")
+    main(colours)
