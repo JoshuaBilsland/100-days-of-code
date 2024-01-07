cdd2909679f237f10a25890bbd3046d1badd5be4
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-07 08:41:12 +0000

    feat(019): add clear_screen

--- day-019/project-019_etch_a_sketch/main.py
+++ day-019/project-019_etch_a_sketch/main.py
@@ -20,12 +20,20 @@ def clockwise():
     tim.setheading(tim.heading() - 5)
 
 
+def clear_screen():
+    tim.penup()
+    tim.clear()
+    tim.home()
+    tim.pendown()
+
+
 def main():
     screen.listen()
     screen.onkey(move_forwards, "w")
     screen.onkey(move_backwards, "s")
     screen.onkey(counter_clockwise, "a")
     screen.onkey(clockwise, "d")
+    screen.onkey(clear_screen, "c")
     screen.exitonclick()
 
 
