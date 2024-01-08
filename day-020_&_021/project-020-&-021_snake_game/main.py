baf16760d4765dfee1455b59f632bff732a875e2
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-08 15:29:47 +0000

    feat(020&021): setup screen

---
+++ day-020_&_021/project-020-&-021_snake_game/main.py
@@ -0,0 +1,14 @@
+from turtle import Screen, Turtle
+
+
+def main():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("My Snake Game")
+
+    screen.exitonclick()
+
+
+if __name__ == "__main__":
+    main()
