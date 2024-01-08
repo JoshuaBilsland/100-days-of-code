4aa19945786cf9ece9958066914251a1bc580a32
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-08 15:36:51 +0000

    feat(020&021): create initial snake

--- day-020_&_021/project-020-&-021_snake_game/main.py
+++ day-020_&_021/project-020-&-021_snake_game/main.py
@@ -7,6 +7,13 @@ def main():
     screen.bgcolor("black")
     screen.title("My Snake Game")
 
+    starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+    for pos in starting_positions:
+        snake_segment = Turtle("square")
+        snake_segment.color("white")
+        snake_segment.goto(pos)
+
+
     screen.exitonclick()
 
 
