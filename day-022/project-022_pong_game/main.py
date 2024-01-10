94739e59807a411b1b0c25d32fcbe55aa6952a04
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-10 15:21:23 +0000

    feat(022): setup screen

---
+++ day-022/project-022_pong_game/main.py
@@ -0,0 +1,13 @@
+from turtle import Screen, Turtle
+
+
+def main():
+    screen = Screen()
+    screen.bgcolor("black")
+    screen.setup(width=600, height=600)
+    screen.title("Pong")
+    screen.exitonclick()
+    
+
+if __name__ == "__main__":
+    main()
