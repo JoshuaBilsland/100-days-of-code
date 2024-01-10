ce797a8c6ce053ca9c24848f4b4ae6ee401a3654
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-10 15:50:36 +0000

    refactor(022): add paddle class

--- day-022/project-022_pong_game/main.py
+++ day-022/project-022_pong_game/main.py
@@ -1,14 +1,6 @@
 from turtle import Screen, Turtle
 from functools import partial
-
-def go_up(paddle):
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down(paddle):
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+from paddle import Paddle
 
 
 def main():
@@ -18,16 +10,14 @@ def main():
     screen.title("Pong")
     screen.tracer(0)
 
-    paddle = Turtle()
-    paddle.shape("square")
-    paddle.color("white")
-    paddle.shapesize(stretch_wid=5, stretch_len=1)
-    paddle.penup()
-    paddle.goto(350, 0)
+    l_paddle = Paddle((-350, 0))
+    r_paddle = Paddle((350, 0))
 
     screen.listen()
-    screen.onkey(partial(go_up, paddle), "Up")
-    screen.onkey(partial(go_down, paddle), "Down")
+    screen.onkey(r_paddle.go_up, "Up")
+    screen.onkey(r_paddle.go_down, "Down")
+    screen.onkey(l_paddle.go_up, "w")
+    screen.onkey(l_paddle.go_down, "s")
 
     running = True
     while running:
