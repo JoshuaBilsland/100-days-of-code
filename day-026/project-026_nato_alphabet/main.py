dab897b7febf0bd2f71d3b34051b02a3fb8a47c7
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-21 16:46:55 +0000

    refactor(026): use dict comp

--- day-026/project-026_nato_alphabet/main.py
+++ day-026/project-026_nato_alphabet/main.py
@@ -22,35 +22,11 @@ for (index, row) in student_data_frame.iterrows():
 
 ############################### MY CODE ###############################
 # TODO 1. Create a dictionary in this format:
-nato_alphabet = {
-    "A": "Alfa",
-    "B": "Bravo",
-    "C": "Charlie",
-    "D": "Delta",
-    "E": "Echo",
-    "F": "Foxtrot",
-    "G": "Golf",
-    "H": "Hotel",
-    "I": "India",
-    "J": "Juliett",
-    "K": "Kilo",
-    "L": "Lima",
-    "M": "Mike",
-    "N": "November",
-    "O": "Oscar",
-    "P": "Papa",
-    "Q": "Quebec",
-    "R": "Romeo",
-    "S": "Sierra",
-    "T": "Tango",
-    "U": "Uniform",
-    "V": "Victor",
-    "W": "Whiskey",
-    "X": "X-ray",
-    "Y": "Yankee",
-    "Z": "Zulu"
-}
+data = pandas.read_csv("day-026/project-026_nato_alphabet/nato_phonetic_alphabet.csv")
 
+nato_alphabet = {row.letter:row.code for (index, row) in data.iterrows()}
+
+print(nato_alphabet)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
