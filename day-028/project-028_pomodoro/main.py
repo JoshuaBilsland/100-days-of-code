4f39b56109c5000bdfc2f60d1a83922ee6c8de87
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-23 15:28:28 +0000

    feat(028): add countdown mechanism

--- day-028/project-028_pomodoro/main.py
+++ day-028/project-028_pomodoro/main.py
@@ -14,6 +14,11 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -27,8 +32,10 @@ title_label.grid(column=1, row=0)
 canvas = Canvas(width=205, height=224, bg=YELLOW, highlightthickness=0)
 bk_image = PhotoImage(file="day-028/project-028_pomodoro/tomato.png")
 canvas.create_image(100, 112, image=bk_image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.grid(column=1, row=1)
+
+count_down(5)
 
 start_button = Button(text="Start", highlightthickness=0)
 start_button.grid(column=0, row=2)
