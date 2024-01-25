4f79c696577efdf52b961a392d2d1877ea10fa69
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-25 18:20:18 +0000

    feat(029): save entry to file

--- day-029/project-029_password_manager/main.py
+++ day-029/project-029_password_manager/main.py
@@ -2,6 +2,14 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_entry():
+    with open("data.txt", "a") as file:
+        website = website_entry.get()
+        email = email_entry.get()
+        password = password_entry.get()
+        file.write(f"{website} | {email} | {password}\n")
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -17,11 +25,13 @@ website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 website_entry = Entry(width=55)
 website_entry.grid(row=1, column=1, columnspan=2, sticky="w")
+website_entry.focus()
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
 email_entry = Entry(width=55)
 email_entry.grid(row=2, column=1, columnspan=2, sticky="w")
+email_entry.insert(0, "my@email.com")
 
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
@@ -30,6 +40,6 @@ password_entry.grid(row=3, column=1, sticky="w")
 
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=46)
+add_button = Button(text="Add", width=46, command=save_entry)
 add_button.grid(row=4, column=1, columnspan=2, sticky="w")
 window.mainloop()
