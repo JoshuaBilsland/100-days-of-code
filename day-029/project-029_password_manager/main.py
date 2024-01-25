af05587f9496fb14c5babeaba83f60a23a10ede0
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-01-25 18:45:14 +0000

    refactor(029): password generator

--- day-029/project-029_password_manager/main.py
+++ day-029/project-029_password_manager/main.py
@@ -1,34 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
-import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
-password_list = []
+    password_letters = [random.choice(letters) for i in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for i in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for i in range(nr_numbers)]
 
-for char in range(nr_letters):
-  password_list.append(random.choice(letters))
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
 
-for char in range(nr_symbols):
-  password_list += random.choice(symbols)
+    password = "".join(password_list)
 
-for char in range(nr_numbers):
-  password_list += random.choice(numbers)
-
-random.shuffle(password_list)
-
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your password is: {password}")
+    password_entry.insert(0, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_entry():
@@ -88,7 +81,7 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=36)
 password_entry.grid(row=3, column=1, sticky="w")
 
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=46, command=save_entry)
 add_button.grid(row=4, column=1, columnspan=2, sticky="w")
