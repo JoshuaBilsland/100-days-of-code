88615e2ea8269bc5e2e9ebd4d58ec3848fb8a71b
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-04-23 08:16:50 +0100

    feat(035): check for rain

--- day-035/project-035_rain_alert/main.py
+++ day-035/project-035_rain_alert/main.py
@@ -10,14 +10,29 @@ def get_api_key():
         print("ERROR: .config file does not exist.")
         exit()
 
+
+def check_for_rain(json_data):
+    will_rain = False
+    for hour in json_data["list"]:
+        condition_code = hour["weather"][0]["id"]
+        if int(condition_code) < 700:
+            will_rain = True
+    if will_rain:
+        print("Bring an umbrella")
+
+
 if __name__ == "__main__":
     os.environ["API_KEY"] = get_api_key()
     OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
     api_parameters = {
         "lon": 51.507351,
         "lat": -0.127758,
-        "appid": os.environ["API_KEY"]
+        "appid": os.environ["API_KEY"],
+        "cnt": 4
     }
 
     response = requests.get(OWM_ENDPOINT, params=api_parameters)
-    print(response)
+    response.raise_for_status()
+
+    weather_json = response.json()
+    check_for_rain(weather_json)
