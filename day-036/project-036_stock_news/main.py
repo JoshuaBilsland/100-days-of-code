d9d87bd321c8c99cc98efdf82a4d169bec2e6489
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-04-24 09:30:32 +0100

    feat(036): complete TODO 5

--- day-036/project-036_stock_news/main.py
+++ day-036/project-036_stock_news/main.py
@@ -7,16 +7,18 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 
-def get_api_key():
+def get_api_key(line_num):
     try:
-        with open('day-035/project-035_rain_alert/.config', 'r') as configFile:
-            return configFile.readline()
+        with open('day-036/project-036_stock_news/.config', 'r') as configFile:
+            lines = configFile.readlines()
+            return lines[line_num]
     except FileNotFoundError:
         print("ERROR: .config file does not exist.")
         exit()
 
 
-API_KEY = get_api_key()
+STOCK_API_KEY = get_api_key(0)
+NEWS_API_KEY = get_api_key(1)
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -25,11 +27,11 @@ API_KEY = get_api_key()
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
-    "apikey": API_KEY
+    "apikey": STOCK_API_KEY
 }
-response = requests.get(STOCK_ENDPOINT, params=stock_params)
-response.raise_for_status()
-json_data = response.json()["Time Series (Daily)"]
+stock_response = requests.get(STOCK_ENDPOINT, params=stock_params)
+stock_response.raise_for_status()
+json_data = stock_response.json()["Time Series (Daily)"]
 list_data = [value for (key, value) in json_data.items()]
 yesterday_data = list_data[0]
 yesterday_closing_price = yesterday_data["4. close"]
@@ -37,7 +39,7 @@ yesterday_closing_price = yesterday_data["4. close"]
 
 # TODO 2. - Get the day before yesterday's closing stock price
 day_before_yesterday_data = list_data[1]
-day_before_yesterday_closing_price = list_data["4. close"]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 
 
 # TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
@@ -49,32 +51,43 @@ percentage_difference = (positive_difference / float(yesterday_closing_price)) *
 
 
 # TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+if percentage_difference > 5:
+    # print("Get news")
 
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+    ## STEP 2: https://newsapi.org/
+    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    news_response.raise_for_status()
+    news_json = news_response.json()
+    print(news_json)
 
 # TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+
 
 # TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
 
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
+    #to send a separate message with each article's title and description to your phone number.
 
 # TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
 
-# TODO 9. - Send each article as a separate message via Twilio. 
+# TODO 9. - Send each article as a separate message via Twilio.
 
 
 
-#Optional TODO: Format the message like this: 
+#Optional TODO: Format the message like this:
 """
 TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 or
 "TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
 
