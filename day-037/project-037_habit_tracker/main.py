7eca06cc772deedf214dd134b22cba58e870b5ee
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-04-26 09:08:52 +0100

    feat(037): create user account

--- day-037/project-037_habit_tracker/main.py
+++ day-037/project-037_habit_tracker/main.py
@@ -12,15 +12,21 @@ def get_api_key(line_num):
 
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
-PIXELA_TOKEN = get_api_key()
+PIXELA_TOKEN = get_api_key(0)
 
 
 def main():
     user_params = {
         "token": PIXELA_ENDPOINT,
+        "username": "joshb109",
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes"
     }
-    requests.post()
+    response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+    print(response.text)
+    response.raise_for_status()
 
 
-if __name__ == "__main":
+
+if __name__ == "__main__":
     main()
