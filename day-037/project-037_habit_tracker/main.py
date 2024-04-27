c72e98e104269eca2f4e30ba400343c8200b88ab
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-04-27 10:02:31 +0100

    feat(037): POST a pixel to the graph

--- day-037/project-037_habit_tracker/main.py
+++ day-037/project-037_habit_tracker/main.py
@@ -1,5 +1,5 @@
 import requests
-
+from datetime import datetime
 
 def get_api_key(line_num):
     try:
@@ -15,8 +15,7 @@ def main():
     PIXELA_ENDPOINT = "https://pixe.la/v1/users"
     PIXELA_TOKEN = get_api_key(0)
     PIXELA_USERNAME = get_api_key(1)  # Stores username in .config
-    print(PIXELA_TOKEN)
-    print(PIXELA_USERNAME)
+    PIXELA_GRAPH_ID = "graph1"
 
     user_params = {
         "token": PIXELA_TOKEN,
@@ -26,12 +25,12 @@ def main():
     }
 
     # CREATING USER ACCOUNT
-    #response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
-    #print(response.text)
+    response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+    print(response.text)
 
     # CREATING USER GRAPH
     graph_config = {
-        "id": "graph1",
+        "id": PIXELA_GRAPH_ID,
         "name": "Pages Read Graph",
         "unit": "Pages",
         "type": "int",
@@ -44,13 +43,23 @@ def main():
     }
 
     GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{PIXELA_USERNAME}/graphs"
-    #response = requests.post(
-    #    url=GRAPH_ENDPOINT,
-    #    json=graph_config,
-    #    headers=pixela_headers
-    #)
-    #print(response.text)
+    response = requests.post(
+        url=GRAPH_ENDPOINT,
+        json=graph_config,
+        headers=pixela_headers
+    )
+    print(response.text)
 
+    # POST A PIXEL
+    POST_PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{PIXELA_USERNAME}/graphs/{PIXELA_GRAPH_ID}"
+    today = datetime.now()
+
+    pixel_params = {
+        "date": today.strftime("%Y%m%d"),
+        "quantity": "5"
+    }
+    response = requests.post(url=POST_PIXEL_ENDPOINT, json=pixel_params, headers=pixela_headers)
+    print(response.text)
 
 if __name__ == "__main__":
     main()
