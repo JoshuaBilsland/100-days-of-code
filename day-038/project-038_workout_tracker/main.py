fdda1e94e615df0481a5ecb0f472e82c1e4aa185
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-04-29 15:54:56 +0100

    feat(038): get exercise stats with NLP query

--- day-038/project-038_workout_tracker/main.py
+++ day-038/project-038_workout_tracker/main.py
@@ -1,6 +1,9 @@
+import requests
+
+
 def get_api_key(line_num):
     try:
-        with open('day-037/project-037_habit_tracker/.config', 'r') as configFile:
+        with open('day-038/project-038_workout_tracker/.config', 'r') as configFile:
             lines = configFile.readlines()
             return lines[line_num].strip()
     except FileNotFoundError:
@@ -9,8 +12,33 @@ def get_api_key(line_num):
 
 
 def main():
+    WEIGHT_KG = YOUR_WEIGHT
+    HEIGHT_CM = YOUR_HEIGHT
+    AGE = YOUR_AGE
+
     APP_ID = get_api_key(0)
     API_KEY = get_api_key(1)
+
+    EXERCISE_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+    user_exercise_input = input("Tell me which exercises you did: ")
+
+    request_headers = {
+        "Content-Type": "application/json",
+        "x-app-id": APP_ID,
+        "x-app-key": API_KEY
+    }
+    params = {
+        "query": user_exercise_input,
+        "weight_kg": WEIGHT_KG,
+        "height_cm": HEIGHT_CM,
+        "age": AGE
+    }
+    response = requests.post(url=EXERCISE_ENDPOINT,
+                             headers=request_headers,
+                             json=params)
+    response.raise_for_status()
+    print(response.json())
 
 
 if __name__ == "__main__":
