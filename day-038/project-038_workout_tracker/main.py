35005a3bab64f64625b992811e890f9640a8ea1f
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-04-29 17:37:44 +0100

    feat(038): post to google sheets

--- day-038/project-038_workout_tracker/main.py
+++ day-038/project-038_workout_tracker/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 
 def get_api_key(line_num):
@@ -12,14 +13,15 @@ def get_api_key(line_num):
 
 
 def main():
-    WEIGHT_KG = YOUR_WEIGHT
-    HEIGHT_CM = YOUR_HEIGHT
-    AGE = YOUR_AGE
+    WEIGHT_KG = 65
+    HEIGHT_CM = 175
+    AGE = 19
 
     APP_ID = get_api_key(0)
     API_KEY = get_api_key(1)
 
     EXERCISE_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+    SHEET_ENDPOINT = get_api_key(2)
 
     user_exercise_input = input("Tell me which exercises you did: ")
 
@@ -38,7 +40,32 @@ def main():
                              headers=request_headers,
                              json=params)
     response.raise_for_status()
-    print(response.json())
+    json_response = response.json()
+
+    # Add row to Google Sheets with Sheety API
+    current_date = datetime.now()
+    current_date = current_date.strftime("%d/%m/%Y")
+    current_time = datetime.now()
+    current_time = current_time.strftime("%X")
+
+    for exercise in json_response["exercises"]:
+        exercise_name = exercise["name"]
+        duration_min = exercise["duration_min"]
+        nf_calories = exercise["nf_calories"]
+
+        sheet_params = {
+            "workout": {
+                "date": current_date,
+                "time": current_time,
+                "exercise": exercise_name.title(),
+                "duration": duration_min,
+                "calories": nf_calories
+            }
+        }
+        sheet_response = requests.post(url=SHEET_ENDPOINT,
+                                       headers=request_headers,
+                                       json=sheet_params)
+        sheet_response.raise_for_status()
 
 
 if __name__ == "__main__":
