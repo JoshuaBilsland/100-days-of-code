2c97b6a1dfdeb52f4d477b698d39437f6533dbd0
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-05-28 19:11:39 +0100

    feat(046): auth with spotify

--- day-046/project-046_spotify_playlist/main.py
+++ day-046/project-046_spotify_playlist/main.py
@@ -1,5 +1,17 @@
 from bs4 import BeautifulSoup
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy
 import requests
+
+
+def get_api_key(line_num):
+    try:
+        with open('day-046/project-046_spotify_playlist/.config', 'r') as configFile:
+            lines = configFile.readlines()
+            return lines[line_num].strip()
+    except FileNotFoundError:
+        print("ERROR: .config file does not exist.")
+        exit()
 
 
 def main():
@@ -11,6 +23,19 @@ def main():
     soup = BeautifulSoup(response.text, 'html.parser')
     song_name_lines = soup.select("li ul li h3")
     song_names = [song.getText().strip() for song in song_name_lines]
+
+    sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=get_api_key(0),
+        client_secret=get_api_key(1),
+        show_dialog=True,
+        cache_path="token.txt",
+        username=get_api_key(2), 
+    )
+)
+    user_id = sp.current_user()["id"]
 
 
 if __name__ == "__main__":
