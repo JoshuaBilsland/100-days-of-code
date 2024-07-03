7500081594915b00861fb9cb8ce12f2cfbcc7406
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-07-03 07:02:02 +0100

    feat(049): automatically login

--- day-049/project-049_linkedin_automation/main.py
+++ day-049/project-049_linkedin_automation/main.py
@@ -1,5 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+def get_config_key(line_num):
+    try:
+        with open('day-049/project-049_linkedin_automation/.config', 'r') as configFile:
+            lines = configFile.readlines()
+            return lines[line_num].strip()
+    except FileNotFoundError:
+        print("ERROR: .config file does not exist.")
+        exit()
 
 
 def main():
@@ -8,7 +19,19 @@ def main():
     chrome_options.add_experimental_option("detach", True)
 
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get("")
+    driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3946620182&keywords=python%20developer&origin=JOBS_HOME_SEARCH_BUTTON&refresh=true")
+
+    sign_in = driver.find_element(By.CSS_SELECTOR, value="body > div.base-serp-page > header > nav > div > a.nav__button-secondary.btn-md.btn-secondary-emphasis")
+    sign_in.click()
+
+    email_entry = driver.find_element(By.ID, value="username")
+    password_entry = driver.find_element(By.ID, value="password")
+    email = get_config_key(0)
+    password = get_config_key(1)
+    email_entry.send_keys(email)
+    password_entry.send_keys(password, Keys.ENTER)
+
+    input()
 
 
 if __name__ == "__main__":
